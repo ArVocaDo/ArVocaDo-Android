0235594b1131c5c8770b03391e20d3ac032028dc
yunakim2 <98_02@naver.com>
2020-11-05 03:47:58 +0900

    UPDATE : Login,SignUp Activity Create

---
+++ app/src/main/java/com/example/arvocado_android/ui/signup/SignupEmailActivity.kt
@@ -0,0 +1,12 @@
+package com.example.arvocado_android.ui.signup
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.arvocado_android.R
+
+class SignupEmailActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup_email)
+    }
+}
