e37e957595aa7e24d3e35c39d44de43370353c2c
yunakim2 <98_02@naver.com>
2020-11-22 15:45:56 +0900

    UPDATE : SpeechTestActivity 추가

---
+++ app/src/main/java/com/example/arvocado_android/ui/sound/SpeechTestActivity.kt
@@ -0,0 +1,17 @@
+package com.example.arvocado_android.ui.sound
+
+import android.os.*
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import com.example.arvocado_android.R
+import java.lang.ref.WeakReference
+
+class SpeechTestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_speech_test)
+
+    }
+
+}
