5f6db4c17cfe0ec85bcd1a6d330c474f0989fdf9
EunJin Lee <sue9897@naver.com>
2020-11-26 12:14:41 +0900

    UPDATE : CameraActivity.kt

--- app/src/main/java/com/example/arvocado_android/ui/camera/CameraActivity.kt
+++ app/src/main/java/com/example/arvocado_android/ui/camera/CameraActivity.kt
@@ -1,17 +1,32 @@
 package com.example.arvocado_android.ui.camera
 
+import android.Manifest
+import android.app.Activity
+import android.content.pm.PackageManager
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.example.arvocado_android.R
 import com.example.arvocado_android.network.AuthManager
 import com.example.arvocado_android.network.NetworkManager
 import com.example.arvocado_android.util.safeEnqueue
+import kotlinx.android.synthetic.main.activity_camera.*
+import okhttp3.internal.EMPTY_REQUEST
 import org.koin.android.ext.android.inject
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
 
 class CameraActivity : AppCompatActivity() {
     var c_idx : Int = 0
     private val networkManager : NetworkManager by inject()
     private val authManager : AuthManager by inject()
+    private lateinit var cameraExecutor: ExecutorService
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_camera)
@@ -20,7 +35,48 @@ class CameraActivity : AppCompatActivity() {
         initCamera()
     }
     private fun initCamera() {
+        if(allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS
+            )
+        }
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
 
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener(Runnable {
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(viewFinder.createSurfaceProvider())
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+            } catch(e: Exception) {
+                Log.e(TAG, "binding falied", e)
+            }
+        }, ContextCompat.getMainExecutor(this))
+    }
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
     private fun requestCategoryWord() {
 
@@ -37,5 +93,10 @@ class CameraActivity : AppCompatActivity() {
                 }
             )
 
+    }
+    companion object {
+        private const val TAG = "ARVOCADO_CAMERAACTIVITY"
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+        private const val REQUEST_CODE_PERMISSIONS = 10
     }
 }
