49f17655a7d38eb9eecbaa9574c6090ab0411532
EunJin Lee <sue9897@naver.com>
2020-11-27 17:35:15 +0900

    UPDATE : Arcore2Activity.kt

--- app/src/main/java/com/example/arvocado_android/ui/camera/Arcore2Activity.kt
+++ app/src/main/java/com/example/arvocado_android/ui/camera/Arcore2Activity.kt
@@ -1,81 +1,102 @@
 package com.example.arvocado_android.ui.camera
-import android.graphics.Color
+
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
 import android.widget.Toast
-import androidx.fragment.app.Fragment
+import androidx.appcompat.app.AlertDialog
 import com.example.arvocado_android.R
 import com.google.ar.core.Anchor
 import com.google.ar.sceneform.AnchorNode
 import com.google.ar.sceneform.Node
-import com.google.ar.sceneform.collision.Box
-import com.google.ar.sceneform.math.Vector3
 import com.google.ar.sceneform.rendering.ModelRenderable
-import com.google.ar.sceneform.rendering.ViewRenderable
 import com.google.ar.sceneform.ux.ArFragment
 import com.google.ar.sceneform.ux.TransformableNode
-import kotlinx.android.synthetic.main.activity_main.*
-import java.util.concurrent.CompletableFuture
+import com.google.ar.sceneform.ux.BaseArFragment
 
 class Arcore2Activity : AppCompatActivity() {
-
     private lateinit var arFragment: ArFragment //The ARFragment where you detect and tap on plane
     val viewNodes = mutableListOf<Node>() // List of all nodes.
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_arcore2)
+        Toast.makeText(this, "arcore2 activity is started ", Toast.LENGTH_SHORT).show()
+        arFragment= supportFragmentManager.findFragmentById(R.id.arFragment) as ArFragment
 
-        arFragment= ArFragment() //Initialise the ARFragment
-        //arFragment = supportFragmentManager.findFragmentById(R.id.arFragment)
+        arFragment.setOnTapArPlaneListener(BaseArFragment.OnTapArPlaneListener { hitResult, plane, motionEvent ->
+            val anchor: Anchor = hitResult.createAnchor()
 
+            ModelRenderable.builder()
+                .setSource(this, Uri.parse("kiwi.sfb"))
+                .build()
+                .thenAccept{ addModelToScene(anchor, it)}
+                .exceptionally {
+                    val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+                    builder.setMessage(it.localizedMessage)
+                        .show()
+                    return@exceptionally null
+                }
+        })
         // When you tap on a detected plane, a callback function is called which adds a Node to the Scene
-        arFragment.setOnTapArPlaneListener{hitResult,x,y ->
-            loadModel{modelRenderable ->
-                addNodeToScene(hitResult.createAnchor(), modelRenderable)
-            }
-        }
+//        arFragment.setOnTapArPlaneListener{hitResult,x,y ->
+//            loadModel{modelRenderable ->
+//                addNodeToScene(hitResult.createAnchor(), modelRenderable)
+//            }
+//        }
 
     }
 
+//    override fun onDestroy() {
+//        if(session != null) {
+//            session!!.close()
+//            session = null
+//        }
+//        super.onDestroy()
+//    }
     // Uses the ModelRenderable and the ViewRenderable objects to add a Node to the Scene
-    private fun addNodeToScene(
-        anchor: Anchor,
-        modelRenderable: ModelRenderable
-    ) {
-        val anchorNode = AnchorNode(anchor)
-        val modelNode = TransformableNode(arFragment.transformationSystem).apply {
-            renderable = modelRenderable
-            setParent(anchorNode)
-            arFragment.arSceneView.scene.addChild(anchorNode)
-            select()
-        }
+//    private fun addNodeToScene(
+//        anchor: Anchor,
+//        modelRenderable: ModelRenderable
+//    ) {
+//        val anchorNode = AnchorNode(anchor)
+//        val modelNode = TransformableNode(arFragment.transformationSystem).apply {
+//            renderable = modelRenderable
+//            setParent(anchorNode)
+//            arFragment.arSceneView.scene.addChild(anchorNode)
+//            select()
+//        }
+//
+//        modelNode.setOnTapListener { _, _ ->
+//            if(!modelNode.isTransforming) {
+//
+//            }
+//        }
+//    }
 
-        modelNode.setOnTapListener { _, _ ->
-            if(!modelNode.isTransforming) {
-
-            }
-        }
+    private fun addModelToScene(anchor: Anchor, it: ModelRenderable?) {
+        val anchorNode: AnchorNode = AnchorNode(anchor)
+        val transform: TransformableNode = TransformableNode(arFragment.transformationSystem)
+        transform.setParent(anchorNode)
+        transform.renderable = it
+        arFragment.arSceneView.scene.addChild(anchorNode)
+        transform.select()
     }
-
 
     //This functions load the model
-    private fun loadModel(callback: (ModelRenderable, ) -> Unit) {
-        val modelRenderable = ModelRenderable.builder()
-            .setSource(this, R.raw.kiwi)
-            .build()
-
-        CompletableFuture.allOf(modelRenderable)
-            .thenAccept {
-                callback(modelRenderable.get())
-            }
-            .exceptionally {
-                Toast.makeText(this, "Error loading model: $it", Toast.LENGTH_LONG).show()
-                null
-            }
-    }
-
-
+//    private fun loadModel(callback: (ModelRenderable, ) -> Unit) {
+//        val modelRenderable = ModelRenderable.builder()
+//            .setSource(this, R.raw.kiwi)
+//            .build()
+//
+//        CompletableFuture.allOf(modelRenderable)
+//            .thenAccept {
+//                callback(modelRenderable.get())
+//            }
+//            .exceptionally {
+//                Toast.makeText(this, "Error loading model: $it", Toast.LENGTH_LONG).show()
+//                null
+//            }
+//    }
 
 }
